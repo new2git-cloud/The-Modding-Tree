c39bb6a682b85d6e3d92f4fadea0ea10ed4054fe
new2git-cloud <macristel.alfonso@gmail.com>
2025-06-27 14:06:22 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,7 +13,7 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Made by new2git-cloud",
+	name: "new2git-cloud",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
