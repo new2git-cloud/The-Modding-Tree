e62e5cbc82877c42f3ac927624f9e059ca263c31
new2git-cloud <macristel.alfonso@gmail.com>
2025-06-27 17:07:23 +0800

    js/layers.js

--- js/layers.js
+++ js/layers.js
@@ -27,13 +27,9 @@ addLayer("p", {
     layerShown(){return true}
 })
 upgrades: {
-11; {
-title: "The First Upgrade"
-description: "Gain 1 Point per second."
-cost: new Decimal("1")
-}
-}
 
-{if (hasUpgrade('p', 11)) gain = gain.plus(1)
+11; {
+
+}
 
 }
