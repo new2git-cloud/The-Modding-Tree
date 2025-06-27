9b17e6efd0a4f914ef4dcfa8b6d94a22ade32c7b
new2git-cloud <macristel.alfonso@gmail.com>
2025-06-27 16:08:23 +0800

    js/layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("p", {
+addLayer("P", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -33,6 +33,6 @@ description: "Gain 1 Point per second."
 cost: new Decimal("1")
 }
 
-{if(hasUpgrade('p', 11)) gain = gain.plus(1)
+{if(hasUpgrade("P", 11)) gain = gain.plus(1)
 
 }
